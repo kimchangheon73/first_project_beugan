3b12dd8a26f76c07c8ecf249142784a935361fa2
박준형 <alsdn9846@naver.com>
2022-04-05 15:59:41 +0900

    됬다 씨발

--- config/DB.js
+++ config/DB.js
@@ -2,11 +2,11 @@ const mysql = require('mysql')  // mysql 프로그램을 가져온게 아니라 
 
 // mysql연결 (ip주소, userid, password, port번호, 연결할데이터베이스)
 let conn = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '1234',
-    port: '3306',
-    database: 'beaugan_item'
+    host: 'project-db-stu.ddns.net',
+    user: 'campus_g_0325_6',
+    password: 'smhrd6',
+    port: '3307',
+    database: 'campus_g_0325_6'
 })
 
 // DB연결 
